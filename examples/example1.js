b7dc90accdb0eeb68f0753036313dac438ba11fe
ActionNerd <pi@raspberrypi-Office.(none)>
2015-02-02 05:13:36 +0000

    readaxes now working nominally

--- examples/example1.js
+++ examples/example1.js
@@ -1,14 +1,25 @@
-var l3gd20 = require('l3gd20');
+var l3gd20 = require('../lib/l3gd20');
+
+console.log(l3gd20);
 
 var l3g  = new l3gd20(); // This is a lowercase "L" not a one "1"
-
+console.log(l3g);
 var gyro = l3g.rategyro();
+var gyroIntCount = 0;
 
-gyro.readAxes(function(err,axes){
-    if(err){
-        console.log("Error reading Accelerometer Axes : " + err);
-    }
-    if (axes) {
-        console.log(axes);
-    }
-});
+
+
+var intObj = setInterval(function() {
+	gyro.readAxes(function(err,axes) {
+		if(err){
+			console.log("Error reading Gyro Axes : " + err);
+		}
+		if (axes) {
+			console.log(axes);
+		}
+	});
+	gyroIntCount++;
+	if (gyroIntCount == 2000) {
+		clearInterval(intObj);
+	}
+}, 200);
